6f87709b66f1f2eadb8fba2427459cdab7d96d97
AlanGrcM <89883446+AlanGrcM@users.noreply.github.com>
2022-02-08 15:50:47 -0600

    Create Led_PWM.py
    Nuevo programa

---
+++ GPIO/Led_PWM.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM) #Librerios del GPIO de la raspberrypi4
+
+GPIO.setup(24, GPIO.OUT) #Pin 24 definido como salida
+
+Led= GPIO.PWM(24, 100) #objeto en el pin 24 que gobierne la modulación PWM
+Led.start(100)
+
+Led.ChangeDutyCycle( x ) #Cambio del ciclo de trabajo 
+print("X es un valor entre 0 y 100")
+
+while True:
+    for i in range(100,-1,-1):
+        Led.ChangeDutyCycle(100 - i)
+        time.sleep(0.02)           
+
+    print("Ciclo completo")
