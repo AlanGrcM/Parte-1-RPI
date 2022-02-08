598544e183399169c21f8bf34b7cfeb708b38f12
AlanGrcM <89883446+AlanGrcM@users.noreply.github.com>
2022-02-08 16:10:42 -0600

    Update Led_PWM.py
    cambio PIN 24-22

--- GPIO/Led_PWM.py
+++ GPIO/Led_PWM.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM) #Librerios del GPIO de la raspberrypi4
 
-GPIO.setup(24, GPIO.OUT) #Pin 24 definido como salida
+GPIO.setup(22, GPIO.OUT) #Pin 22 definido como salida
 
-Led= GPIO.PWM(24, 100) #objeto en el pin 24 que gobierne la modulación PWM
+Led= GPIO.PWM(22, 100) #objeto en el pin 22 que gobierne la modulación PWM
 Led.start(100)
 
 Led.ChangeDutyCycle( 50 ) #Cambio del ciclo de trabajo 
